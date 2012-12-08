6cf1a36d08d0390f940b3cccb3d496be2245c41e
zeiv <fxb9500@gmail.com>
2012-12-08 15:43:38 -0600

    Changed water flow disable threshold to a time-based system rather than dryness value

--- main.c
+++ main.c
@@ -10,22 +10,71 @@
 // Licensed under the MIT license
 //======================================================================================
 
+//#include <msp430g2553.h>
+//volatile unsigned int i;
+//void main(void)
+//{
+//WDTCTL = WDT_ADLY_1000; // WDT 1s interval timer
+//IE1 |= WDTIE; // Enable WDT interrupt
+//P1DIR = 0xFF; // All P1.x outputs
+//P1OUT = 0; // All P1.x reset
+//P2DIR = 0xFF; // All P2.x outputs
+//P2OUT = 0; // All P2.x reset
+//BCSCTL3 = XCAP_3; //12.5pF cap- setting for 32768Hz crystal
+//// An immediate Osc Fault will occur next
+//IE1 |= OFIE; // Enable Osc Fault
+//while(1)
+//{
+//P1OUT ^= 0x01; // Toggle P1.0 using exclusive-OR
+//_BIS_SR(LPM3_bits + GIE); // Enter LPM3 w/interrupt
+//}
+//}
+//#pragma vector=WDT_VECTOR
+//__interrupt void watchdog_timer (void)
+//{
+//_BIC_SR_IRQ(LPM3_bits); // Clear LPM3 bits from 0(SR)
+//}
+//#pragma vector=NMI_VECTOR
+//__interrupt void nmi_ (void)
+//{
+//do
+//{
+//IFG1 &= ~OFIFG; // Clear OSCFault flag
+//for (i = 0xFFF; i > 0; i--); // Time for flag to set
+//P1OUT ^= 0x01; // Toggle P1.0 using exclusive-OR
+//}
+//while (IFG1 & OFIFG); // OSCFault flag still set?
+//IE1 |= OFIE; // Enable Osc Fault
+//}
+
 #include "msp430G2553.h"
 
 // Pin Configuration (Defaults in parentheses):
 #define WATER BIT6								// Output for water flow control (P1.6)
 #define PROBE BIT5								// Input for dryness detection probe (P1.1 = A1)
-#define MIN_DRY 0x220							// ADC Input level at which to turn on water flow (0x220)
-#define HYDR 0x150								// How much the plant should be watered --CANNOT EXCEED MIN_DRY!-- (0x150)
+#define MIN_DRY 0x0100							// ADC Input level at which to turn on water flow (0x220)
+#define W_TIME 30								// How long to water the plany in seconds
+#define DELAY 0.5								// Minimum time to wait between waterings in minutes
 #define BTN BIT3								// Button for bypassing automatic watering
 
+int flow = 0;
 int bypass;
+int i = 0;
+int delay = (DELAY*60);
 
 void main(void)
 {
-	WDTCTL = WDTPW + WDTHOLD;					// Stops WD Timer
-	ADC10CTL0 = ADC10SHT_2 + ADC10ON;			// ADC On
 //	ADC10CTL1 = CONSEQ_2;
+	WDTCTL = WDT_ADLY_1000; // WDT 1s interval timer
+	IE1 |= WDTIE; // Enable WDT interrupt
+	P1DIR = 0xFF; // All P1.x outputs
+	P1OUT = 0; // All P1.x reset
+	P2DIR = 0xFF; // All P2.x outputs
+	P2OUT = 0; // All P2.x reset
+	BCSCTL3 = XCAP_3; //12.5pF cap- setting for 32768Hz crystal
+	ADC10CTL0 = ADC10SHT_2 + ADC10ON;			// ADC On
+	// An immedate Osc Fault will occur next
+	IE1 |= OFIE; // Enable Osc Fault
 	switch ( PROBE ) {							// Sets ADC channel to match PROBE
 	case BIT0:
 		ADC10CTL1 |= INCH_0;
@@ -84,43 +133,57 @@ void main(void)
 	ADC10AE0 |= PROBE;							// Enables analog input on PROBE
 	ADC10CTL0 |= ENC;
 
-	P1DIR |= WATER + BIT0;
+	P1DIR |= WATER;
 	P1DIR &= ~BTN;
 	P1IN |= BTN;
 	P1IE |= BTN;
+	P1OUT %= ~WATER;							// Water flow initally off
 
 	__enable_interrupt();
 
 	for (;;) {
 		ADC10CTL0 |= ADC10SC;					// Collects data
-		if (bypass == 0x1) {
-			P1OUT |= BIT0;
-			P1OUT &= ~WATER;
-		}
-		else if (bypass == 0) {
-			P1OUT &= ~BIT0;
-		}
-
-		if (ADC10MEM > MIN_DRY && bypass == 0) {				// When the dryness rises above MIN_DRY and Bypass is disabled,
+		if (ADC10MEM > MIN_DRY && delay >= (DELAY*60)) {				// When the dryness rises above MIN_DRY and Bypass is disabled,
 			P1OUT |= WATER;						// Turns on water flow
+			flow = 1;
+			delay = 0;
+			_BIS_SR(LPM3_bits + GIE); // Enter LPM3 w/interrupt
+
 		}
-		else if (ADC10MEM < MIN_DRY - HYDR) {	// When the dryness falls below MIN_DRY minus HYDR
-			P1OUT &= ~WATER;					// Turns off water flow
-		}
+//		else if (ADC10MEM < MIN_DRY - HYDR) {	// When the dryness falls below MIN_DRY minus HYDR
+//			P1OUT &= ~WATER;					// Turns off water flow
+//		}
 	}
 
 }
 
-
-#pragma vector=PORT1_VECTOR
-__interrupt void Port_1(void) {
-	switch ( bypass ) {
-	case 0:
-		bypass = 1;
-		break;
-	case 1:
-		bypass = 0;
-		break;
+#pragma vector=WDT_VECTOR
+__interrupt void watchdog_timer (void)
+{
+	if (flow == 1) {
+		if (i < W_TIME) {
+			i++;
+		}
+		else {
+			i = 0;
+			flow = 0;
+			P1OUT &= ~WATER;
+			delay = 0;
+			_BIC_SR_IRQ(LPM3_bits); // Clear LPM3 bits from 0(SR)
+		}
 	}
-	P1IFG &= ~BTN;
+	else if (flow == 0) {
+		delay++;
+	}
+}
+#pragma vector=NMI_VECTOR
+__interrupt void nmi_ (void)
+{
+do
+{
+IFG1 &= ~OFIFG; // Clear OSCFault flag
+for (i = 0xFFF; i > 0; i--); // Time for flag to set
+}
+while (IFG1 & OFIFG); // OSCFault flag still set?
+IE1 |= OFIE; // Enable Osc Fault
 }
